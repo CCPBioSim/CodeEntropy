5410f53d0b731d99d7ba1642b3582993a1fc1f37
skfegan <sarah.fegan@stfc.ac.uk>
2023-08-18 17:08:33 +0100

    adding principal axis option for residue axis selection. Needs testing

--- CodeEntropy/FunctionCollection/CustomFunctions.py
+++ CodeEntropy/FunctionCollection/CustomFunctions.py
@@ -5,7 +5,7 @@ from bitarray import bitarray
 from CodeEntropy import ClassCollection as CC 
 from CodeEntropy.FunctionCollection import Utils
 
-##TODO replace custom cross_product with numpy function
+## DEPRECIATED replaced custom cross_product with numpy.cross() function
 def cross_product(arg_v1, arg_v2):
 	""" 
 	Returns cross product of two vectors using analytical expressions 
